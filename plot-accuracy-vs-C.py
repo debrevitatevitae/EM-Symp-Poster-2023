c78a61dc6aec32eb18437ae4f919ae0a2a191d25
debrevitatevitae <giotb92@gmail.com>
2023-09-28 17:46:13 +0200

    Add plot script

---
+++ plot-accuracy-vs-C.py
@@ -0,0 +1,15 @@
+"""Date: 28-9-23
+Author: Giorgio Tosti Balducci
+
+Description: plots the mean accuracies of the cross validation runs for different values of the constraint penalty C (in SVMs). Different lines correspond to different values of the kernel hyperparameters (e.g. \gamma for RBF).
+"""
+
+import pandas as pd
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
